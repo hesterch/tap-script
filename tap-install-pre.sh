b680211c7f6e394239d2f8037a08cd2d752c137e
anilvmware <70898535+anilvmware@users.noreply.github.com>
2023-03-16 15:46:40 -0400

    Create tap-install-pre.sh

---
+++ tap-install-pre.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#read -p "Enter the Tanzu network username: " tanzunetusername
+#read -p "Enter the Tanzu network password: " tanzunetpassword
+#export INSTALL_REGISTRY_USERNAME=$tanzunetusername
+#export INSTALL_REGISTRY_PASSWORD=$tanzunetpassword
+#export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
+
+#echo "################# Create namespace ###########################"
+#kubectl create ns tap-install
+
+#echo "################# Create secret tap-registry ##############################"
+#tanzu secret registry add tap-registry --username ${INSTALL_REGISTRY_USERNAME} --password ${INSTALL_REGISTRY_PASSWORD} --server ${INSTALL_REGISTRY_HOSTNAME} --export-to-all-namespaces --yes --namespace tap-install
+
+echo "############# Adding Tanzu Application Platform package repository to the cluster ####################"
+tanzu package repository add tanzu-tap-repository --url registry.tanzu.vmware.com/tanzu-application-platform/tap-packages:1.3.2 --namespace tap-install
+tanzu package repository get tanzu-tap-repository --namespace tap-install
+echo "############# List the available packages ####################"
+tanzu package available list --namespace tap-install
+
+echo "############### TAP 1.3.2 Install   ##################"
+tanzu package install tap -p tap.tanzu.vmware.com -v 1.3.2 --values-file $HOME/tap-script/tap-values.yaml -n tap-install
+tanzu package installed list -A
