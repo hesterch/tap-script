aca513a9fa13449793ab8f9805502e6001212ba5
anilvmware <70898535+anilvmware@users.noreply.github.com>
2023-03-16 21:43:05 -0400

    Update image-copy.sh

--- image-copy.sh
+++ image-copy.sh
@@ -3,7 +3,7 @@
 export INSTALL_REGISTRY_USERNAME=taptestdemoacr
 export INSTALL_REGISTRY_PASSWORD=yy2n1woGlAOP6fWOrqb+R9Ev4M7eY52eU6KZEcDzwz+ACRDEJ0Q+
 export INSTALL_REGISTRY_HOSTNAME=taptestdemoacr.azurecr.io
-export TAP_VERSION=1.4.1
+export TAP_VERSION=1.4.2
 export INSTALL_REPO=tap14
 imgpkg copy -b registry.tanzu.vmware.com/tanzu-application-platform/tap-packages:${TAP_VERSION} --to-repo ${INSTALL_REGISTRY_HOSTNAME}/${INSTALL_REPO}/tap-packages
 imgpkg copy -b registry.tanzu.vmware.com/tanzu-application-platform/full-tbs-deps-package-repo:1.7.4 \
